efd29b850e8429183b3f7da4a6f768e8d311976e
DarthChurros <tnarayan00@stuy.edu>
2020-02-03 07:37:19 -0500

    wrote ppm maker

--- main.c
+++ main.c
@@ -4,7 +4,22 @@
 #include <unistd.h>
 
 int main() {
-  open("image.ppm", O_WRONLY | O_CREAT | O_TRUNC, 644);
+  FILE* img = fopen("image.ppm", "w");
+  fprintf(img, "P3\n256 256 255\n");
+
+  int i;
+  int j;
+  int k;
+  for (i = 0; i < 256; i++) {
+    for (j = 0; j < 256; j++) {
+      for (k = 0; k < 3; k++) {
+        fprintf(img, "%d ", (int)((k*i-j)%256));
+      }
+    }
+    fprintf(img, "\n");
+  }
+
+  fclose(img);
 
   return 0;
 }
