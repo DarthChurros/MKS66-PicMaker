e716d4f971e605b24495afaf4d04b343d6416405
DarthChurros <tnarayan00@stuy.edu>
2020-02-03 07:56:21 -0500

    fixed negative issue

--- main.c
+++ main.c
@@ -3,9 +3,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+int mod(int a, int b);
+
 int main() {
   FILE* img = fopen("image.ppm", "w");
-  fprintf(img, "P3\n512 512 255\n");
+  fprintf(img, "P3\n512 256 255\n");
 
   int i;
   int j;
@@ -13,7 +15,7 @@ int main() {
   for (i = 0; i < 512; i++) {
     for (j = 0; j < 512; j++) {
       for (k = 0; k < 3; k++) {
-        fprintf(img, "%d ", (int)(((k*i-j)/2)%256));
+        fprintf(img, "%d ", (int)abs(((k*i-j)/2)%256));
       }
     }
     fprintf(img, "\n");
