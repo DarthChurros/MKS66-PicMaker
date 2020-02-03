cb5afe50af37e17aa9a240fa0f60c1629a5d9962
DarthChurros <tnarayan00@stuy.edu>
2020-02-03 07:39:48 -0500

    increased resolution

--- main.c
+++ main.c
@@ -5,15 +5,15 @@
 
 int main() {
   FILE* img = fopen("image.ppm", "w");
-  fprintf(img, "P3\n256 256 255\n");
+  fprintf(img, "P3\n512 512 255\n");
 
   int i;
   int j;
   int k;
-  for (i = 0; i < 256; i++) {
-    for (j = 0; j < 256; j++) {
+  for (i = 0; i < 512; i++) {
+    for (j = 0; j < 512; j++) {
       for (k = 0; k < 3; k++) {
-        fprintf(img, "%d ", (int)((k*i-j)%256));
+        fprintf(img, "%d ", (int)(((k*i-j)/2)%256));
       }
     }
     fprintf(img, "\n");
