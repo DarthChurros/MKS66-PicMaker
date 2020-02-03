37b2dbd566ee0e5be204bf8e3dc420c33e277778
DarthChurros <tnarayan00@stuy.edu>
2020-02-03 08:11:13 -0500

    updated algorithm

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int mod(int a, int b);
 
 int main() {
   FILE* img = fopen("image.ppm", "w");
-  fprintf(img, "P3\n512 256 255\n");
+  fprintf(img, "P3\n512 512 255\n");
 
   int i;
   int j;
@@ -15,7 +15,7 @@ int main() {
   for (i = 0; i < 512; i++) {
     for (j = 0; j < 512; j++) {
       for (k = 0; k < 3; k++) {
-        fprintf(img, "%d ", (int)abs(((k*i-j)/2)%256));
+        fprintf(img, "%d ", (int)abs(((k*i+j)/2)%256));
       }
     }
     fprintf(img, "\n");
